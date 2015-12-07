58d741ac4b07cf8d8b8155513eb672f5e6e03a49
Your Name Here <mescgroup@yahoo.com>
2015-12-07 09:53:47 -0700

    add more

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import org.junit.Test;
+/**
+ * Created by robin.cox on 12/7/2015.
+ */
+public class TestMain extends TestCase
+{
+    @Test
+    public void testMainHooHah()
+    {
+        System.out.println("Hoo hah");
+    }
+}
